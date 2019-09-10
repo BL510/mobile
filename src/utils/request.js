b93074d66407af26bcea1f7b8aafdca96fa2ed6a
bl <15245108475@163.com>
2019-09-10 10:25:28 +0800

    json-bigint

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,9 +1,19 @@
 import axios from 'axios'
+import JSONbig from 'json-bigint'
 // 创建一个instance实例，设置不同的baseURL
 const instance = axios.create({
   timeout: 1000,
   baseURL: 'http://ttapi.research.itcast.cn'
 })
+
+// 获取服务器返回的数据，并且是在处理数据之前使用
+instance.defaults.transformResponse = [function (data) {
+  try {
+    return JSONbig.parse(data)
+  } catch (err) {
+    return data
+  }
+}]
 // 请求拦截器
 instance.interceptors.request.use(function (config) {
   return config
