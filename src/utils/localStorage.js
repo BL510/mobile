edbe19ca0dfc617e5fab3d6b49b84a5fa5e6ac24
bl <15245108475@163.com>
2019-09-11 02:23:54 +0800

    本地存储的封装

---
+++ src/utils/localStorage.js
@@ -0,0 +1,10 @@
+export const setItem = (key, item) => {
+  // item 如果是对象，转换成字符串
+  window.localStorage.setItem(key, JSON.stringify(item))
+}
+export const getItem = (key) => {
+  return JSON.parse(window.localStorage.getItem(key))
+}
+export const removeItem = (key) => {
+  window.localStorage.removeItem(key)
+}
