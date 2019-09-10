304108900a8f61bf0c7fc7e2cf429810037c3464
bl <15245108475@163.com>
2019-09-11 03:40:33 +0800

    VeeValidate基本使用

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -6,7 +6,12 @@
      />
      <!-- 输入框 -->
      <van-cell-group>
-        <van-field v-model="user.mobile" left-icon="phone" placeholder="请输入手机号码" />
+        <van-field
+        v-validate="required"
+        name="mobile"
+        :error-message="errors.first('mobile')"
+        clearable
+        v-model="user.mobile" left-icon="phone" placeholder="请输入手机号码" />
         <van-field v-model="user.code" left-icon="lock" placeholder="请输入验证码">
         <van-button slot="button" type="default" size="small">发送验证码</van-button>
         </van-field>
