a57f682b45c762f591b53c7432eb6ab732e87e37
bl <15245108475@163.com>
2019-09-12 16:45:10 +0800

    请求拦截器设置token

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import JSONbig from 'json-bigint'
+import store from '@/store'
 // 创建一个instance实例，设置不同的baseURL
 const instance = axios.create({
   timeout: 5000,
@@ -16,6 +17,12 @@ instance.defaults.transformResponse = [function (data) {
 }]
 // 请求拦截器
 instance.interceptors.request.use(function (config) {
+  // 判断是否有登录状态
+  // 注意：此处是js模块，不是组件，要导入store
+  if (store.state.user) {
+    // 如果有登录状态请求的时候，自动携带token
+    config.headers.Authorization = `Bearer ${store.state.user.token}`
+  }
   return config
 }, function (error) {
   return Promise.reject(error)
