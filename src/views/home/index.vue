23f53a8c305858fb62b0b9afb9057842cd80ce4b
bl <15245108475@163.com>
2019-09-12 17:37:19 +0800

    首页布局-1

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
-      Home
+    <!-- 导航头 -->
+    <van-nav-bar title="黑马头条" />
+    <!-- 频道列表 -->
+    <van-tabs>
+      <van-tab v-for="index in 8" :title="'标签 ' + index " :key="index">内容{{ index }}</van-tab>
+    </van-tabs>
   </div>
 </template>
 
@@ -11,5 +16,4 @@ export default {
 </script>
 
 <style>
-
 </style>
