9b9b1a79f049aab08866e6392a53e2ced55c08c2
bl <15245108475@163.com>
2019-09-24 01:55:44 +0800

    频道管理的界面

--- src/views/home/components/ChannelEdit.vue
+++ src/views/home/components/ChannelEdit.vue
@@ -2,10 +2,40 @@
   <van-popup
   :value="value"
   @input="$emit('input',$event)"
-  position="bottom"
-  :style="{ height:'80%' }"
+  position="left"
+  :style="{ height:'100%'}"
   >
-   hello
+   <van-cell icon="cross" @click="$emit('input',false)" />
+   <!-- 我的频道 -->
+   <van-cell title="我的频道" label="点击进入频道">
+     <van-button
+      round
+      type="danger"
+      size="mini"
+     >编辑</van-button>
+   </van-cell>
+   <van-grid>
+     <van-grid-item
+      v-for="value in 8"
+      :key="value"
+      text="文字"
+     >
+     <van-icon
+      slot="icon"
+      class="close-icon"
+      name="close"
+     />
+     </van-grid-item>
+   </van-grid>
+   <!-- 频道推荐 -->
+   <van-cell title="频道推荐" label="点击添加频道"/>
+     <van-grid>
+       <van-grid-item
+        v-for="value in 8"
+        :key="value"
+        text="文字"
+       />
+     </van-grid>
   </van-popup>
 </template>
 
@@ -26,6 +56,10 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+ .close-icon{
+   position:absolute;
+   right:0;
+   top:0;
+ }
 </style>
